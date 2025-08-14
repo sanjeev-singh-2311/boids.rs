1130be50c43bced228215a3c9b46ca81810cce17
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2025-08-14 14:22:38 +0530

    use of Rc<RefCell<>> for Boids

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use boids::Boid;
+use boids::{Boid, BoidRef};
 use boids::{FLOCK_SIZE, WIN_HEIGHT, WIN_WIDTH};
 use raylib::prelude::*;
 
@@ -12,20 +12,22 @@ fn main() {
         .title("Boids")
         .build();
 
-    let mut global_flock: VecDeque<Boid> = (0..FLOCK_SIZE).map(|_| Boid::new()).collect();
+    // let mut global_flock: VecDeque<Boid> = (0..FLOCK_SIZE).map(|_| Boid::new()).collect();
+    let global_flock: Vec<BoidRef> = (0..FLOCK_SIZE).map(|_| Boid::new()).collect();
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::BLACK);
 
-        while let Some(mut boid) = global_flock.pop_front() {
-            boid.update(&global_flock);
-            global_flock.push_back(boid);
+        for boid in global_flock.iter() {
+            let mut curr_boid = boid.borrow_mut();
+            (*curr_boid).update(&global_flock);
         }
 
-        for boid in &global_flock {
-            boid.draw(&mut d, 5.0, Color::WHITE);
+        for boid in global_flock.iter() {
+            let curr_boid = boid.borrow_mut();
+            (*curr_boid).draw(&mut d, 5.0, Color::WHITE);
         }
     }
 }
