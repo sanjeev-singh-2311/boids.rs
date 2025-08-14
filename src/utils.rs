895f44368c304f957a273863f09c137a05682838
Shresth Prasad <shresthprasad7@gmail.com>
2025-08-15 01:24:27 +0530

    fix: Prevent boids' color from being too close to bg

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,8 @@
 use rand::random_range;
 use raylib::{color::Color, math::Vector2};
 
+use crate::config::BACKGROUND_COLOR as BG;
+
 pub fn move_vec_towards(vec: Vector2, target: Vector2, step: f32) -> Vector2 {
     let diff = target - vec;
     let dist_sq = diff.length_sqr();
@@ -13,8 +15,21 @@ pub fn move_vec_towards(vec: Vector2, target: Vector2, step: f32) -> Vector2 {
 }
 
 pub fn get_random_color() -> Color {
-    let hue = random_range(0.0..=360.0);
     let sat = 0.9;
     let val = 0.9;
-    Color::color_from_hsv(hue, sat, val)
+
+    loop {
+        let hue = random_range(0.0..=360.0);
+        let color = Color::color_from_hsv(hue, sat, val);
+
+        let dist = (
+              (color.r as f32 - BG.r as f32).powi(2)
+            + (color.g as f32 - BG.g as f32).powi(2)
+            + (color.b as f32 - BG.b as f32).powi(2)
+        ).sqrt();
+
+        if dist >= 100.0 {
+            return color;
+        }
+    }
 }
